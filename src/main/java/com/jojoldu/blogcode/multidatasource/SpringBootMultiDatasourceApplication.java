4fe2e07fc05c6ed94c8d51caaca8de4ae51d2f66
이동욱 <jojoldu@gmail.com>
2019-02-11 10:34:37 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/blogcode/multidatasource/SpringBootMultiDatasourceApplication.java
@@ -0,0 +1,14 @@
+package com.jojoldu.blogcode.multidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiDatasourceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/jojoldu/blogcode/multidatasource/SpringBootMultiDatasourceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jojoldu.blogcode.multidatasource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMultiDatasourceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
